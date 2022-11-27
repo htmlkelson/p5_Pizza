77ea1e2804e943d2e15805e747e28a76fd8c3f4e
htmlkelson <118143122+htmlkelson@users.noreply.github.com>
2022-11-27 13:41:27 -0500

    Add files via upload

---
+++ empty-example/sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+  createCanvas(500,500)
+  // six-digit hexadecimal RGB notation
+background('#222222');
+describe('canvas with black background');
+   stroke("#895C40");
+  fill("#D8BD1F");
+  ellipse(width/2,height/2,200,200);
+  
+}
+
+function draw() {
+  fill(0);
+textSize(30);
+textFont('Georgia');
+textFont('Helvetica');
+text('Pepperoni Pizza', 130, 100);
+}
+
+function mousePressed(){
+  if(mouseX)
+  fill("#DC1313")
+  ellipse(mouseX,mouseY,26,26);
+  }
